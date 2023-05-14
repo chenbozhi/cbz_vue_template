7c76508dd2274f08f03de70039dcb286c972710c
banjieming <1127230996@qq.com>
2023-05-14 20:10:18 +0800

    1. 禁止双指放大，静止双击放大

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
-
 <template>
 
     <router-view/>
 </template>
 
 <script setup>
+// 禁用双指放大
+document.documentElement.addEventListener('touchstart', function (event) {
+    if (event.touches.length > 1) {
+        event.preventDefault();
+    }
+}, {
+    passive: false
+
+});
+// 禁用双击放大
+let lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+    let now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+        event.preventDefault();
+    }
+    lastTouchEnd = now;
+}, {
+    passive: false
+})
 </script>
 
 <style scoped>
