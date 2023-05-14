069a65c55765267ab13b47896cd415dce068c152
banjieming <1127230996@qq.com>
2023-05-14 15:54:34 +0800

    1. 按需引入element-plus，并配置国际化，为中文

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 
 <template>
+    <el-config-provider :locale="zhCn">
 
+    </el-config-provider>
 </template>
 
 <script setup>
+import { ElConfigProvider } from 'element-plus'
+
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
 </script>
 
 <style scoped>
