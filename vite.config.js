0b718818e2ba6a90225c24d9db1715801cdd0a4d
banjieming <1127230996@qq.com>
2023-05-14 15:54:34 +0800

    1. 按需引入element-plus，并配置国际化，为中文
    (cherry picked from commit 069a65c55765267ab13b47896cd415dce068c152)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 引入vant自动引入组件
 import Components from 'unplugin-vue-components/vite';
 import {VantResolver} from 'unplugin-vue-components/resolvers';
+
+// 引入element-plus自动引入组件
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         Components({
-            resolvers: [VantResolver()],
+            resolvers: [
+                VantResolver(),
+                ElementPlusResolver()
+            ],
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
         }),
     ],
 })
